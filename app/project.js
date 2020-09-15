b7abb5adfb6658ead5ad1bc1adb7b0d4e137d82a
Andreia Pena <andreia.pena@pix.fr>
2020-09-15 15:09:18 +0200

    up

--- app/project.js
+++ app/project.js
@@ -1,26 +1,7 @@
 class Project {
 
-    constructor(){
-    }
-
-    test(value){
-    }
-
-    testFunction(){
-        this.mockFunction()
-        return this.stubFunction()
-    }
-
-    mockFunction(){
-        return -1;
-    }
-
-    stubFunction(){
-        return 1
-    }
-
-    exceptionFunction(){
-         throw new Error('Exception Catch');
+    fizzBuzz(value){
+        return value;
     }
 
 }
--- tests/project_spec.js
+++ tests/project_spec.js
@@ -3,19 +3,37 @@ const expect = require('chai').expect;
 const sinon = require('sinon')
 const Project = require('../app/project.js');
 
-describe('Project Test', () => {
+describe('Project', () => {
 
     const project = new Project()
 
-    context('Expect', () => {
-        it('should return 1', () => {
+    context('#fizzBuzz', () => {
+        it('should return 1 when value equal 1', () => {
             //given
-            const value = 1
+            const value = 1;
             //when
-            const fizzbuzz =  project.test(value)
-
+            const resultFizzBuzz =  project.fizzBuzz(value);
             //then
-            expect(fizzbuzz).to.equal(1)
-        })
+            expect(resultFizzBuzz).to.equal(1);
+        });
+
+        it('should return 2 when value equal 2', () => {
+            //given
+            const value = 2;
+            //when
+            const resultFizzBuzz =  project.fizzBuzz(value);
+            //then
+            expect(resultFizzBuzz).to.equal(2);
+        });
+
+        it('should return "Fizz" when value equal 3', () => {
+            //given
+            const value = 2;
+            //when
+            const resultFizzBuzz =  project.fizzBuzz(value);
+            //then
+            expect(resultFizzBuzz).to.equal(2);
+        });
+
     })
 })
